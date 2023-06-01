b00e53544761eb11bc762c10b8202ef7bd29be04
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-01 15:48:41 -0500

    Add places

--- friends.rb
+++ friends.rb
@@ -3,3 +3,8 @@ arrays = ["Mark", "Nate", "Richard", "Tim", "Bobby"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Japan", "Korea", "New Zealand", "Italy", "Israel"]
+
+places.pop
+puts places
